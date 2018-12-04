3028fe7e8883f39003e7c496fdb6e27843090491
Tim Deignan <tdeignan@6river.com>
2018-12-04 12:08:59 -0500

    feat: BlackboardRefFactory

--- lib/BlackboardRef.ts
+++ lib/BlackboardRef.ts
@@ -2,15 +2,3 @@ export interface BlackboardRef {
 	readonly uuid: string,
 	readonly name: string,
 }
-
-export function blackboardRefToString(blackboardRef: BlackboardRef) {
-	return blackboardRef.uuid + '-' + blackboardRef.name;
-}
-
-// could be needed for enhanced serialization of Blackboard instances
-// export function stringToBlackboardRef(str: string): BlackboardRef {
-// 	return {
-// 		uuid: str.substring(0, 36),
-// 		name: str.substring(37),
-// 	};
-// }
--- lib/InMemoryMapBlackboard.ts
+++ lib/InMemoryMapBlackboard.ts
@@ -1,19 +1,31 @@
 import {Blackboard} from './Blackboard';
-import {BlackboardRef, blackboardRefToString} from './BlackboardRef';
+import {BlackboardRef} from './BlackboardRef';
 
 export class InMemoryMapBlackboard implements Blackboard {
 	private readonly state: Map<string, any> = new Map();
 
 	public get<T>(blackboardRef: BlackboardRef): T {
-		return this.state.get(blackboardRefToString(blackboardRef));
+		return this.state.get(this.blackboardRefToString(blackboardRef));
 	}
 
 	public put(blackboardRef: BlackboardRef, value: any) {
-		this.state.set(blackboardRefToString(blackboardRef), value);
+		this.state.set(this.blackboardRefToString(blackboardRef), value);
 		return this;
 	}
 
 	public delete(blackboardRef: BlackboardRef) {
-		return this.state.delete(blackboardRefToString(blackboardRef));
+		return this.state.delete(this.blackboardRefToString(blackboardRef));
 	}
+
+	private blackboardRefToString(blackboardRef: BlackboardRef) {
+		return blackboardRef.uuid + '-' + blackboardRef.name;
+	}
+
+	// could be needed for enhanced serialization of Blackboard instances
+	// private stringToBlackboardRef(str: string): BlackboardRef {
+	// 	return {
+	// 		uuid: str.substring(0, 36),
+	// 		name: str.substring(37),
+	// 	};
+	// }
 }
