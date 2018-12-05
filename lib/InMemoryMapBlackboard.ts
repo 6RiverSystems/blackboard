d0ede58c4471f51866bab1a6944e28e61ed99d2a
Tim Deignan <tdeignan@6river.com>
2018-12-05 12:49:37 -0500

    feat: make stateReadable publiuc instead of private

--- lib/InMemoryMapBlackboard.ts
+++ lib/InMemoryMapBlackboard.ts
@@ -32,8 +32,12 @@ export class InMemoryMapBlackboard implements Blackboard {
 		return this.state.delete(ref.uuid);
 	}
 
-	// handy for viewing in the debugger and logging
-	private get stateReadable(): Object {
+	// handy method for viewing in the debugger and logging
+	// Q: why is this method public?
+	// A: cfs_models depends on this method. Therefore, changes to this method should be considered breaking
+	// Q: doesn't exposing entire state of the blackboard compromise component isolation?
+	// A: no, because this method is not in the Blackboard interface
+	public get stateReadable(): Object {
 		return [...this.state.entries()].reduce((acc, next) => {
 			const uuid = next[0];
 			const name = next[1][0].name;
