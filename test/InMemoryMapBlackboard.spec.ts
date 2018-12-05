5884e0c301069dd5043ab95666f50a71b36c6d4c
Tim Deignan <tdeignan@6river.com>
2018-12-05 13:57:13 -0500

    feat: add test for stateReadable

--- test/InMemoryMapBlackboard.spec.ts
+++ test/InMemoryMapBlackboard.spec.ts
@@ -2,7 +2,7 @@ import {assert} from 'chai';
 import {InMemoryMapBlackboard} from '../lib/InMemoryMapBlackboard';
 
 describe('InMemoryMapBlackboard', function() {
-	it('CREATE', function() {
+	it('create', function() {
 		const uut = new InMemoryMapBlackboard();
 		const bbRef = {uuid: '91f2053e-f74c-11e8-8eb2-f2801f1b9fd1', name: 'someName'};
 		const value = 'someValue';
@@ -24,7 +24,7 @@ describe('InMemoryMapBlackboard', function() {
 		assert.isTrue(thrown);
 	});
 
-	it('GET', function() {
+	it('get', function() {
 		const uut = new InMemoryMapBlackboard();
 		const bbRef = {uuid: '91f2053e-f74c-11e8-8eb2-f2801f1b9fd1', name: 'someName'};
 		const value = 'someValue';
@@ -46,7 +46,7 @@ describe('InMemoryMapBlackboard', function() {
 		assert.strictEqual(gottenValue, value);
 	});
 
-	it('TRYGET', function() {
+	it('tryGet', function() {
 		const uut = new InMemoryMapBlackboard();
 		const bbRef = {uuid: '91f2053e-f74c-11e8-8eb2-f2801f1b9fd1', name: 'someName'};
 		const value = 'someValue';
@@ -63,7 +63,7 @@ describe('InMemoryMapBlackboard', function() {
 		assert.strictEqual(gottenValue, value);
 	});
 
-	it('PUT', function() {
+	it('put', function() {
 		const uut = new InMemoryMapBlackboard();
 		const bbRef = {uuid: '91f2053e-f74c-11e8-8eb2-f2801f1b9fd1', name: 'someName'};
 		const value = 'someValue';
@@ -81,7 +81,7 @@ describe('InMemoryMapBlackboard', function() {
 		assert.strictEqual(gottenValue, value);
 	});
 
-	it('DELETE', function() {
+	it('delete', function() {
 		const uut = new InMemoryMapBlackboard();
 		const bbRef = {uuid: '91f2053e-f74c-11e8-8eb2-f2801f1b9fd1', name: 'someName'};
 		const value = 'someValue';
@@ -94,4 +94,52 @@ describe('InMemoryMapBlackboard', function() {
 		deleteResult = uut.delete(bbRef);
 		assert.isFalse(deleteResult);
 	});
+
+	it('stateReadable', function() {
+		const uut = new InMemoryMapBlackboard();
+		const uuid1 = '91f2053e-f74c-11e8-8eb2-f2801f1b9fd1';
+		const name1 = 'someName';
+		const value1 = 'someValue1';
+		const bbRef1 = {uuid: uuid1, name: name1};
+
+		const uuid2 = 'e0562668-f8bb-11e8-8eb2-f2801f1b9fd1';
+		const name2 = 'someName';
+		const value2 = 'someValue2';
+		const bbRef2 = {uuid: uuid2, name: name2};
+
+		assert.deepStrictEqual(uut.stateReadable, {});
+
+		uut.put(bbRef1, value1);
+
+		assert.deepStrictEqual(uut.stateReadable, {[name1]: value1});
+
+		uut.put(bbRef2, value2);
+
+		deepStrictEqualOr(
+			uut.stateReadable, 
+			{
+				[name1]: value1,
+				[`${name2}-${uuid2}`]: value2,
+			}, 
+			{
+				[`${name1}-${uuid1}`]: value1,
+				[name2]: value2,
+			}
+		)
+	});
 });
+
+function deepStrictEqualOr(a: any, ...possibilities: any[]) {
+	for (let i = 0; i < possibilities.length; i++) {
+		const b = possibilities[i];
+		const lastPossibility = (i === possibilities.length - 1);
+		try {
+			assert.deepStrictEqual(a, b);
+			return;
+		} catch(err) {
+			if (lastPossibility) {
+				throw err;
+			}
+		}
+	}
+}
