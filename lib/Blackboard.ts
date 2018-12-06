9e3f289a6113cd5da34d58512e99e7da490f784f
David Durschlag <ddurschlag@6river.com>
2018-12-06 16:04:52 -0500

    fix: better types for FS-5344

--- lib/Blackboard.ts
+++ lib/Blackboard.ts
@@ -3,7 +3,7 @@ import {BlackboardRef} from "./BlackboardRef";
 export interface Blackboard {
 	create<T>(ref: BlackboardRef<T>, value: T): void;
 	get<T>(ref: BlackboardRef<T>): T;
-	tryGet<T>(ref: BlackboardRef<T>): [boolean, T?];
+	tryGet<T>(ref: BlackboardRef<T>): [true, T]|[false, undefined];
 	put<T>(ref: BlackboardRef<T>, value: T): void;
 	delete(ref: BlackboardRef<any>): boolean;
 }
